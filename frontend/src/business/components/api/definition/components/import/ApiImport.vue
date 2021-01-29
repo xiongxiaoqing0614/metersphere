5c18df2b7aabecaf765c16610243dd33f11d2eb2
xiongxiaoqing <xiongxiaoqing@tuhu.cn>
2021-01-29 14:25:19 +0800

    story:41741 add prod sf story: 41741 Add logic to detect if it has prod in hostname story:41958 Add UI for forseti integration story:41741 add prod sf story: 41741 Add logic to detect if it has prod in hostname story:41958 Add UI for forseti integration story:41741 add prod sf story: 41741 Add logic to detect if it has prod in hostname story:41741 add prod sf story: 41741 Add logic to detect if it has prod in hostname story:41741 add prod sf story: 41741 Add logic to detect if it has prod in hostname story:41741 add prod sf story: 41741 Add logic to detect if it has prod in hostname story:41741 add prod sf story: 41741 Add logic to detect if it has prod in hostname

--- frontend/src/business/components/api/definition/components/import/ApiImport.vue
+++ frontend/src/business/components/api/definition/components/import/ApiImport.vue
@@ -174,6 +174,20 @@ export default {
         moduleId: '',
         appId: '',
       },
+      forsetiFormData: {
+        file: undefined,
+        forsetiUrl: '',
+        tuhuAppId: '',
+        modeId: this.$t('commons.not_cover'),
+        moduleId: '',
+      },
+      forsetiFormData: {
+        file: undefined,
+        forsetiUrl: '',
+        tuhuAppId: '',
+        modeId: this.$t('commons.not_cover'),
+        moduleId: '',
+      },
       rules: {},
       currentModule: {},
       fileList: []
