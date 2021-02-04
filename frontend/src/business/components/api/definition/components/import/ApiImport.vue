3b7cd369ebb62631e6435d1a6da456908402a634
xiongxiaoqing <xiongxiaoqing@tuhu.cn>
2021-02-04 20:22:07 +0800

    story:41958 Add UI for forseti integration

--- frontend/src/business/components/api/definition/components/import/ApiImport.vue
+++ frontend/src/business/components/api/definition/components/import/ApiImport.vue
@@ -181,6 +181,13 @@ export default {
         modeId: this.$t('commons.not_cover'),
         moduleId: '',
       },
+      forsetiFormData: {
+        file: undefined,
+        forsetiUrl: '',
+        tuhuAppId: '',
+        modeId: this.$t('commons.not_cover'),
+        moduleId: '',
+      },
       rules: {},
       currentModule: {},
       fileList: []
