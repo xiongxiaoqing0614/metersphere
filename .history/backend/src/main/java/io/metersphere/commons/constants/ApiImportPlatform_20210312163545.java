f465cf4e88ac0316f3d2d9209bcab7388754a9f6
xiongxiaoqing <xiongxiaoqing@tuhu.cn>
2021-03-12 18:22:06 +0800

    revert 41955

---
+++ .history/backend/src/main/java/io/metersphere/commons/constants/ApiImportPlatform_20210312163545.java
@@ -0,0 +1,9 @@
+package io.metersphere.commons.constants;
+
+public enum ApiImportPlatform {
+<<<<<<< HEAD
+    Metersphere, Postman, Swagger2, Plugin, Jmeter, Forseti
+=======
+    Metersphere, Postman, Swagger2, Plugin, Jmeter, Har
+>>>>>>> master
+}
