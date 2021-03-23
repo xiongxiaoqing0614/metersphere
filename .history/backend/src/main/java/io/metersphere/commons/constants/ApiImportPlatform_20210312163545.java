447f4528b0b67a01d0b7877365e6efa828e57fc3
xiongxiaoqing <xiongxiaoqing@tuhu.cn>
2021-03-23 18:28:45 +0800

    remove useless folder

--- .history/backend/src/main/java/io/metersphere/commons/constants/ApiImportPlatform_20210312163545.java
+++
@@ -1,9 +0,0 @@
-package io.metersphere.commons.constants;
-
-public enum ApiImportPlatform {
-<<<<<<< HEAD
-    Metersphere, Postman, Swagger2, Plugin, Jmeter, Forseti
-=======
-    Metersphere, Postman, Swagger2, Plugin, Jmeter, Har
->>>>>>> master
-}
