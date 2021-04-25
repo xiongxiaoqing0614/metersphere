70ecb9f8f2ee49d931384db6e23e35e095644850
熊晓青 <xiongxiaoqing@tuhu.cn>
2021-04-25 16:19:46 +0800

    Merge branch 'importjmx' into 'develop'
    Importjmx
    
    See merge request autotest/metersphere-tuhu!45
    
    (cherry picked from commit dbe3d455710e0faced034efe701ba82ba229fac1)
    
    6e17d394 jmx导入
    78301eb3 导入问题修复
    cba2ff20 jmx导入
    bb8c9c6c 导入问题修复
    d4248d2e Merge branch 'importjmx' of http://gitlab.tuhu.cn/autotest/metersphere-tuhu into importjmx

--- backend/src/main/java/io/metersphere/api/dto/automation/parse/ScenarioImportParserFactory.java
+++ backend/src/main/java/io/metersphere/api/dto/automation/parse/ScenarioImportParserFactory.java
@@ -14,6 +14,8 @@ public class ScenarioImportParserFactory {
             return new MsJmeterParser();
         } else if (StringUtils.equals(ApiImportPlatform.Har.name(), platform)) {
             return new HarScenarioParser();
+        } else if (StringUtils.equals(ApiImportPlatform.JmeterTuhu.name(), platform)){
+            return new MsJmeterTuhuParser();
         }
         return null;
     }
