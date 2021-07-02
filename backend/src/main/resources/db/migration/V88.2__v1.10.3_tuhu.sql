8045d33e7218ffeb4efee55d8b1a9e15599d1409
chenxiaowu <chenxiaowu@tuhu.cn>
2021-07-02 18:16:42 +0800

    fix: 添加索引字段

--- backend/src/main/resources/db/migration/V88.2__v1.10.3_tuhu.sql
+++ backend/src/main/resources/db/migration/V88.2__v1.10.3_tuhu.sql
@@ -1,4 +1,7 @@
 -- 修改 appid 字段长度
 ALTER TABLE tuhu_all_appid MODIFY `app_id` varchar(70) NOT NULL;
 ALTER TABLE tuhu_appid_api_mapping MODIFY `app_id` varchar(70) NOT NULL;
-
+-- 添加索引
+ALTER TABLE test_plan ADD INDEX `idx_name` (`name`);
+-- 添加索引
+ALTER TABLE test_plan_report ADD INDEX `idx_name` (`name`);
